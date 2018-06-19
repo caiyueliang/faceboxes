449001964a0a0aab5f7bdcb7256e148677d008dd
caiyueliang <393900414@qq.com>
2018-06-19 22:33:12 +0800

    update

--- multibox_layer.py
+++ multibox_layer.py
@@ -27,21 +27,28 @@ class MultiBoxLayer(nn.Module):
         '''
         xs:list of 之前的feature map list
         retrun: loc_preds: [N, 21842, 4]
-                conf_preds:[N, 24842, 2]
+                conf_preds:[N, 21842, 2]
         '''
 
         y_locs = []
         y_confs = []
         for i, x in enumerate(xs):
             y_loc = self.loc_layers[i](x)                           # N,anhors*4,H,W
-            N = y_loc.size(0)
-            y_loc = y_loc.permute(0, 2, 3, 1).contiguous()
+            print('MultiBoxLayer_Conv2d', y_loc.size())
+            N = y_loc.size(0)                                       # N分别为：84，4，4
+            print('N', N)
+            y_loc = y_loc.permute(0, 2, 3, 1).contiguous()          # permute 置换|转置
+            print('MultiBoxLayer_permute', y_loc.size())
             y_loc = y_loc.view(N, -1, 4)
+            print('MultiBoxLayer_view', y_loc.size())
             y_locs.append(y_loc)
 
             y_conf = self.conf_layers[i](x)
+            print('MultiBoxLayer_y_conf', y_conf.size())
             y_conf = y_conf.permute(0, 2, 3, 1).contiguous()
+            print('MultiBoxLayer_permute', y_conf.size())
             y_conf = y_conf.view(N, -1, 2)
+            print('MultiBoxLayer_view', y_conf.size())
             y_confs.append(y_conf)
 
         loc_preds = torch.cat(y_locs, 1)
