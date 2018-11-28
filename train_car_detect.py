2bc1906763bf70bcd1b322b42f6491d0fd2c50cd
lijinchao2007 <lijinchao2007@163.com>
2018-11-28 11:38:44 +0800

    update

--- train_car_detect.py
+++ train_car_detect.py
@@ -1,30 +1,20 @@
 # encoding:utf-8
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import torch
 from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
 from torchvision import models
 from torch.autograd import Variable
-
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import transforms as T
 from networks import FaceBox
 from multibox_loss import MultiBoxLoss
 from dataset import ListDataset
 import common
-# import visdom
 import cv2
 import numpy as np
 from encoderl import DataEncoder
 import time
-
-
-use_gpu = torch.cuda.is_available()
-
-re_train = False
-learning_rate = 0.001
-num_epochs = 200
-decay_epoch = 60
-batch_size = 32
 
 
 def show_img(img, boxes):
@@ -42,142 +32,261 @@ def show_img(img, boxes):
     cv2.waitKey(0)
 
 
-def test(net, test_loader, show_info=False):
-    total_test_loss = 0
-    data_encoder = DataEncoder()
+class ModuleTrain:
+    def __init__(self, train_path, test_path, model_file, model, img_size=1024, batch_size=16, lr=1e-3,
+                 re_train=False, best_loss=0.2, use_gpu=False):
+        self.train_path = train_path
+        self.test_path = test_path
+        self.model_file = model_file
+        self.img_size = img_size
+        self.batch_size = batch_size
+        self.re_train = re_train                        # 不加载训练模型，重新进行训练
+        self.best_loss = best_loss                      # 最好的损失值，小于这个值，才会保存模型
+        self.use_gpu = False
 
-    time_start = time.time()
-    # 测试集
-    # for data, target in test_loader:
-    for images, loc_targets, conf_targets in test_loader:
-        images, loc_targets, conf_targets = Variable(images), Variable(loc_targets), Variable(conf_targets)
+        if use_gpu is True:
+            print("gpu available: %s" % str(torch.cuda.is_available()))
+            if torch.cuda.is_available():
+                self.use_gpu = True
+            else:
+                self.use_gpu = False
 
-        if use_gpu:
-            images, loc_targets, conf_targets = images.cuda(), loc_targets.cuda(), conf_targets.cuda()
+        # 模型
+        self.model = model
 
-        # print('images', images.size())
-        loc_preds, conf_preds = net(images)
-        loss = criterion(loc_preds, loc_targets, conf_preds, conf_targets)  # 计算损失
-        total_test_loss += loss.item()
+        if self.use_gpu:
+            print('[use gpu] ...')
+            self.model = self.model.cuda()
 
-        if show_info is True:
-            # print('0 pre_label', loc_preds.size(), conf_preds.size())
-            # print('0 pre_label', loc_preds, conf_preds)
+        # 加载模型
+        if os.path.exists(self.model_file) and not self.re_train:
+            self.load(self.model_file)
 
-            print("loc_preds len: ", len(loc_preds))
-            for i in range(len(loc_preds)):
-                # print('2 pre_label', loc_preds[i].size(), conf_preds[i].size())
-                # print('3 pre_label', loc_preds[i], conf_preds[i])
-                boxes, labels, max_conf = data_encoder.decode(loc_preds[i], conf_preds[i], use_gpu)
-                # print('boxes', boxes)
-                # print('labels', labels)
-                # print('max_conf', max_conf)
+        # RandomHorizontalFlip
+        self.transform_train = T.Compose([
+            # T.Resize((self.img_size, self.img_size)),
+            T.ToTensor(),
+            # T.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5]),
+        ])
 
-                show_img(images[i].permute(1, 2, 0), boxes.cpu().detach().numpy())
-                # show_img(images[i], boxes.cpu().detach().numpy())
+        self.transform_test = T.Compose([
+            # T.Resize((self.img_size, self.img_size)),
+            T.ToTensor(),
+            # T.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5])
+        ])
 
-    time_end = time.time()
-    time_avg = float(time_end - time_start) / float(len(test_loader.dataset))
-    avg_loss = total_test_loss / len(test_loader.dataset)
-    print('[Test] avg_loss: {:.6f} time: {:.6f}\n'.format(avg_loss, time_avg))
-    return avg_loss
+        # Dataset
+        train_label = os.path.join(self.train_path, 'car_detect_train_label.txt')
+        test_label = os.path.join(self.test_path, 'car_detect_test_label.txt')
+        train_dataset = ListDataset(root=self.train_path, list_file=train_label, train=True, transform=self.transform_train)
+        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+        test_dataset = ListDataset(root=self.test_path, list_file=test_label, train=False, transform=self.transform_test)
+        self.test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+
+        self.criterion = MultiBoxLoss()
+
+        self.lr = lr
+        # self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.5)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+
+        pass
+
+    def train(self, epoch, decay_epoch=60, save_best=True):
+        for epoch_i in range(epoch):
+            train_loss = 0.0
+            if epoch_i >= decay_epoch and epoch_i % decay_epoch == 0:                   # 减小学习速率
+                self.lr = self.lr * 0.1
+                self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+
+            print('================================================')
+            for i, (images, loc_targets, conf_targets) in enumerate(self.train_loader):
+                images = Variable(images)
+                loc_targets = Variable(loc_targets)
+                conf_targets = Variable(conf_targets)
+                if self.use_gpu:
+                    images, loc_targets, conf_targets = images.cuda(), loc_targets.cuda(), conf_targets.cuda()
+
+                loc_preds, conf_preds = self.model(images)
+                loss = self.criterion(loc_preds, loc_targets, conf_preds, conf_targets)  # 计算损失
+                train_loss += loss.item()
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+            train_loss /= len(self.train_loader.dataset)
+            print ('[Train] Epoch [%d/%d] average_loss: %.6f lr: %.6f' % (epoch_i + 1, epoch, train_loss, self.lr))
+
+            test_loss = self.test()
+            if save_best is True:
+                if self.best_loss > test_loss:
+                    self.best_loss = test_loss
+                    str_list = self.model_file.split('.')
+                    best_model_file = ""
+                    for str_index in range(len(str_list)):
+                        best_model_file = best_model_file + str_list[str_index]
+                        if str_index == (len(str_list) - 2):
+                            best_model_file += '_best'
+                        if str_index != (len(str_list) - 1):
+                            best_model_file += '.'
+                    self.save(best_model_file)                                  # 保存最好的模型
+
+        self.save(self.model_file)
+
+    def test(self, show_info=False):
+        total_test_loss = 0
+        data_encoder = DataEncoder()
+
+        time_start = time.time()
+        # 测试集
+        # for data, target in test_loader:
+        for images, loc_targets, conf_targets in self.test_loader:
+            images, loc_targets, conf_targets = Variable(images), Variable(loc_targets), Variable(conf_targets)
+
+            if self.use_gpu:
+                images, loc_targets, conf_targets = images.cuda(), loc_targets.cuda(), conf_targets.cuda()
+
+            # print('images', images.size())
+            loc_preds, conf_preds = self.model(images)
+            loss = self.criterion(loc_preds, loc_targets, conf_preds, conf_targets)  # 计算损失
+            total_test_loss += loss.item()
+
+            if show_info is True:
+                # print('0 pre_label', loc_preds.size(), conf_preds.size())
+                # print('0 pre_label', loc_preds, conf_preds)
+
+                print("loc_preds len: ", len(loc_preds))
+                for i in range(len(loc_preds)):
+                    # print('2 pre_label', loc_preds[i].size(), conf_preds[i].size())
+                    # print('3 pre_label', loc_preds[i], conf_preds[i])
+                    boxes, labels, max_conf = data_encoder.decode(loc_preds[i], conf_preds[i], self.use_gpu)
+                    # print('boxes', boxes)
+                    # print('labels', labels)
+                    # print('max_conf', max_conf)
+
+                    show_img(images[i].permute(1, 2, 0), boxes.cpu().detach().numpy())
+                    # show_img(images[i], boxes.cpu().detach().numpy())
+
+        time_end = time.time()
+        time_avg = float(time_end - time_start) / float(len(self.test_loader.dataset))
+        avg_loss = total_test_loss / len(self.test_loader.dataset)
+        print('[Test] avg_loss: {:.6f} time: {:.6f}\n'.format(avg_loss, time_avg))
+        return avg_loss
+
+    def load(self, name):
+        print('[Load model] %s ...' % name)
+        self.model.load_state_dict(torch.load(name))
+        # self.model.load(name)
+
+    def save(self, name):
+        print('[Save model] %s ...' % name)
+        torch.save(self.model.state_dict(), name)
+        # self.model.save(name)
 
 
 if __name__ == '__main__':
-    train_root = os.path.expanduser('~/deeplearning/Data/car_rough_detect/car_detect_train/')
-    train_label = os.path.join(train_root, 'car_detect_train_label.txt')
-    test_root = os.path.expanduser('~/deeplearning/Data/car_rough_detect/car_detect_test/')
-    test_label = os.path.join(test_root, 'car_detect_test_label.txt')
+    train_path = os.path.expanduser('~/deeplearning/Data/car_rough_detect/car_detect_train/')
+    test_path = os.path.expanduser('~/deeplearning/Data/car_rough_detect/car_detect_test/')
 
     common.mkdir_if_not_exist('./weight')
-    # model_file = 'weight/car_detect.pt'                 # 保存的模型名称
-    model_file = 'weight/car_rough_detect.pt'
+    output_model_path = 'weight/car_rough_detect.pt'
 
-    best_loss = 0.1
+    batch_size = 16
+    lr = 0.001
+    img_size = 1024
+    model = FaceBox()
+    model_train = ModuleTrain(train_path=train_path, test_path=test_path, model_file=output_model_path,
+                              model=model, batch_size=batch_size, img_size=img_size, lr=lr, use_gpu=True)
 
-    net = FaceBox()
-    if use_gpu:
-        print('[use gpu] ...')
-        net.cuda()
+    model_train.train(200, 80)
+    # model_train.test(show_img=True)
 
-    # 加载模型
-    if os.path.exists(model_file) and not re_train:
-        print('[load model] ...', model_file)
-        net.load_state_dict(torch.load(model_file))
+    # ================================================================================================
+    # best_loss = 0.1
 
-    criterion = MultiBoxLoss()
+    # net = FaceBox()
+    # if use_gpu:
+    #     print('[use gpu] ...')
+    #     net.cuda()
+    #
+    # # 加载模型
+    # if os.path.exists(model_file) and not re_train:
+    #     print('[load model] ...', model_file)
+    #     net.load_state_dict(torch.load(model_file))
+
+    # criterion = MultiBoxLoss()
 
     # optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0003)
-    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=1e-4)
+    # optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=1e-4)
 
-    # RandomHorizontalFlip
-    transform_train = transforms.Compose([
-        # transforms.Resize(self.img_size),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5]),
-    ])
-    transform_test = transforms.Compose([
-        # T.Resize(self.img_size),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5]),
-    ])
+    # # RandomHorizontalFlip
+    # transform_train = T.Compose([
+    #     # transforms.Resize(self.img_size),
+    #     T.ToTensor(),
+    #     # transforms.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5]),
+    # ])
+    # transform_test = T.Compose([
+    #     # T.Resize(self.img_size),
+    #     T.ToTensor(),
+    #     # transforms.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5]),
+    # ])
 
-    train_dataset = ListDataset(root=train_root, list_file=train_label, train=True, transform=transform_train)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-    test_dataset = ListDataset(root=test_root, list_file=test_label, train=False, transform=transform_test)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+    # train_dataset = ListDataset(root=train_root, list_file=train_label, train=True, transform=transform_train)
+    # train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+    # test_dataset = ListDataset(root=test_root, list_file=test_label, train=False, transform=transform_test)
+    # test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
 
-    print('the dataset has %d images' % (len(train_loader.dataset)))
-    print('the batch_size is %d' % batch_size)
+    # print('the dataset has %d images' % (len(train_loader.dataset)))
+    # print('the batch_size is %d' % batch_size)
 
     # 可视化工具
     # num_iter = 0
     # vis = visdom.Visdom()
     # win = vis.line(Y=np.array([0]), X=np.array([0]))
 
-    net.train()
-    for epoch in range(num_epochs):
-        if epoch >= decay_epoch and epoch % decay_epoch == 0:
-            learning_rate *= 0.1
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = learning_rate
+    # net.train()
+    # for epoch in range(num_epochs):
+    #     if epoch >= decay_epoch and epoch % decay_epoch == 0:
+    #         learning_rate *= 0.1
+    #     for param_group in optimizer.param_groups:
+    #         param_group['lr'] = learning_rate
+    #
+    #     print('\nEpoch [%d/%d] learning_rate:%f' % (epoch + 1, num_epochs, learning_rate))
+    #     total_loss = 0.
+    #
+    #     for i, (images, loc_targets, conf_targets) in enumerate(train_loader):
+    #         images = Variable(images)
+    #         loc_targets = Variable(loc_targets)
+    #         conf_targets = Variable(conf_targets)
+    #         if use_gpu:
+    #             images, loc_targets, conf_targets = images.cuda(), loc_targets.cuda(), conf_targets.cuda()
+    #
+    #         loc_preds, conf_preds = net(images)
+    #         loss = criterion(loc_preds, loc_targets, conf_preds, conf_targets)          # 计算损失
+    #         total_loss += loss.item()
+    #
+    #         optimizer.zero_grad()
+    #         loss.backward()
+    #         optimizer.step()
+    #
+    #     print ('Epoch [%d/%d] average_loss: %.6f' % (epoch + 1, num_epochs, total_loss / len(train_loader.dataset)))
+    #     # num_iter = num_iter + 1
+    #     # vis.line(Y=np.array([total_loss / len(train_loader)]), X=np.array([num_iter]), win=win, update='append')
+    #
+    #     test_loss = test(net, test_loader)
+    #
+    #     # 保存最好的模型
+    #     if best_loss > test_loss:
+    #         best_loss = test_loss
+    #         str_list = model_file.split('.')
+    #         best_model_file = ""
+    #         for str_index in range(len(str_list)):
+    #             best_model_file = best_model_file + str_list[str_index]
+    #             if str_index == (len(str_list) - 2):
+    #                 best_model_file += '_best'
+    #             if str_index != (len(str_list) - 1):
+    #                 best_model_file += '.'
+    #         print('[saving best model] ...', best_model_file)
+    #         torch.save(net.state_dict(), best_model_file)
 
-        print('\nEpoch [%d/%d] learning_rate:%f' % (epoch + 1, num_epochs, learning_rate))
-        total_loss = 0.
-
-        for i, (images, loc_targets, conf_targets) in enumerate(train_loader):
-            images = Variable(images)
-            loc_targets = Variable(loc_targets)
-            conf_targets = Variable(conf_targets)
-            if use_gpu:
-                images, loc_targets, conf_targets = images.cuda(), loc_targets.cuda(), conf_targets.cuda()
-
-            loc_preds, conf_preds = net(images)
-            loss = criterion(loc_preds, loc_targets, conf_preds, conf_targets)          # 计算损失
-            total_loss += loss.item()
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        print ('Epoch [%d/%d] average_loss: %.6f' % (epoch + 1, num_epochs, total_loss / len(train_loader.dataset)))
-        # num_iter = num_iter + 1
-        # vis.line(Y=np.array([total_loss / len(train_loader)]), X=np.array([num_iter]), win=win, update='append')
-
-        test_loss = test(net, test_loader)
-
-        # 保存最好的模型
-        if best_loss > test_loss:
-            best_loss = test_loss
-            str_list = model_file.split('.')
-            best_model_file = ""
-            for str_index in range(len(str_list)):
-                best_model_file = best_model_file + str_list[str_index]
-                if str_index == (len(str_list) - 2):
-                    best_model_file += '_best'
-                if str_index != (len(str_list) - 1):
-                    best_model_file += '.'
-            print('[saving best model] ...', best_model_file)
-            torch.save(net.state_dict(), best_model_file)
-
-    test(net, test_loader, show_info=True)
+    # test(net, test_loader, show_info=False)
